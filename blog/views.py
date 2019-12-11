e6d605b0dbfdaeea6abb9c5390eb85c4ddaa8590
moonpeter <moonpeter@kakao.com>
2019-12-11 21:40:44 +0900

    update

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
+# Create your views here.
 from django.shortcuts import render
 
-# Create your views here.
+
 def post_list(requst):
-    return HttpResponse('Post List!')
+    # Template을 찾을 경로에서
+    # post_list.html을 찾아서
+    # 그 파일을 text로 만들어서 HttpResponse형태로 돌려준다.
+    # 위 기능을 하는 shorcut함수
+    return render(requst, 'post_list.html')
--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print('settings. py의 BASE_DIR:', BASE_DIR)
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+print('settings. py의 TEMPLATES:', TEMPLATES_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +58,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATES_DIR,
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
